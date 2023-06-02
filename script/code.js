8bd4fe17ba0ba638029f6fe48bed91be5e61c68b
Masood-B <masoodbasardien123@gmail.com>
2023-06-02 16:48:06 +0200

    im in pain

--- script/code.js
+++ script/code.js
@@ -8,6 +8,12 @@ const nameDisplay = document.querySelector("#display1")
 const output = document.querySelector("#list")
 // info
 let nameList = []
+let addArray = [{
+    id:1,
+    name:"",
+    dateCreated: new Date(),
+    completed: ""
+}]
 
 addName.addEventListener("click", (e)=>{
     e.preventDefault()
@@ -30,7 +36,18 @@ addName.addEventListener("click", (event)=>{
     nameList = JSON.parse(localStorage.getItem("names"))
     nameList.forEach( (data)=> {
         output.innerHTML += `
-            <li><input type="checkbox">${data}<button class="end-x"> x </button></li>
+            <li class="line"><input type="checkbox">${data}<button id="clear"> x </button></li>
         `
     })
 })
+
+document.querySelector("#clear").
+addEventListener("click", ()=>{
+    let remove = output.innerHTML;
+    output.innerHTML = "";
+})
+
+// document.querySelector(".line").
+// addEventListener("click", ()=>{
+//     output.
+// })
